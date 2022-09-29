d0acb6da3575d7cea64aa99a0d1b23371cf2f877
Young wenjain <jain_y@126.com>
2022-09-29 04:45:55 +0000

    add dioxus crate.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use dioxus::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    dioxus::web::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    let mut count = use_state(&cx, || 0);
+
+    cx.render(rsx!(
+        h1 { "High-Five counter: {count}" }
+        button { onclick: move |_| count += 1, "Up high!" }
+        button { onclick: move |_| count -= 1, "Down low!" }
+    ))
 }
